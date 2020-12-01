8b478a265875eb457caf192d052df27281dc1e37
Marvin Zumbado <marvinzzz@gmail.com>
2020-12-01 12:24:17 -0600

    added support to upload files

--- src/Resource.Api/Resource.Api/Startup.cs
+++ src/Resource.Api/Resource.Api/Startup.cs
@@ -2,11 +2,14 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 using Resource.Api.Models;
+using System.IO;
 using System.Security.Claims;
 
 namespace Resource.Api
@@ -56,6 +59,12 @@ namespace Resource.Api
                 options.EnableEndpointRouting = false;
             })
                 .SetCompatibilityVersion(CompatibilityVersion.Latest);
+
+            services.Configure<FormOptions>(o => {
+                o.ValueLengthLimit = int.MaxValue;
+                o.MultipartBodyLengthLimit = int.MaxValue;
+                o.MemoryBufferThreshold = int.MaxValue;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -82,6 +91,13 @@ namespace Resource.Api
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+            app.UseStaticFiles();
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), @"Resources")),
+                RequestPath = new PathString("/Resources")
             });
 
 
--- src/Spa/oauth-client/src/app/students/details/details.component.ts
+++ src/Spa/oauth-client/src/app/students/details/details.component.ts
@@ -1,9 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+// import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { finalize } from 'rxjs/operators';
 import { AuthService } from '../../core/authentication/auth.service';
 import { StudentsService } from '../students.service';
+
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { HttpEventType, HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-students-details',
@@ -16,8 +20,13 @@ export class DetailsComponent implements OnInit {
   selectedStudent: string;
   student: any;
 
+  public progress: number;
+  public message: string;
+  @Output() public UploadFinished = new EventEmitter();
+
   constructor(private route: ActivatedRoute, private authService: AuthService,
-              private service: StudentsService, private spinner: NgxSpinnerService) {
+              private service: StudentsService, private spinner: NgxSpinnerService,
+              private http: HttpClient) {
   }
 
   ngOnInit() {
@@ -43,6 +52,22 @@ export class DetailsComponent implements OnInit {
 
   }
 
-
+  public uploadFile = (files) => {
+    if (files.length === 0) {
+      return;
+    }
+    const fileToUpload = files[0] as File;
+    const formData = new FormData();
+    formData.append('file', fileToUpload, fileToUpload.name);
+    this.http.post('http://localhost:5050/api/Upload', formData, { reportProgress: true, observe: 'events' })
+      .subscribe(event => {
+        if (event.type === HttpEventType.UploadProgress) {
+          this.progress = Math.round(100 * event.loaded / event.total);
+        } else if (event.type === HttpEventType.Response) {
+          this.message = 'Upload success.';
+          this.UploadFinished.emit(event.body);
+        }
+      });
+  }
 
 }
